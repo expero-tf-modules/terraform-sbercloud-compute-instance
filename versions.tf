4c2975fe4caf798164b3d881dfa831b3135ac787
andreystrb <a.strembitskiy@phrm.tech>
2025-03-28 12:28:35 +0200

    Add provider version

--- versions.tf
+++ versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">= 0.13.1"
-
   required_providers {
     sbercloud = {
       source  = "sbercloud-terraform/sbercloud"
-      version = ">= 1.9.0, < 2.0.0"
+      version = "1.12.8"
     }
   }
 }
